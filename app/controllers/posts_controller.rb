8e0aedd9ab1e1922e0ce9c4fe8995e565ba1fc10
Joao Paulo Soares <smokeonline@gmail.com>
2019-03-06 17:43:15 -0300

    Tests and implementation for retrieving blog posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,18 @@
 class PostsController < ApplicationController
 
+  # GET /posts
+  def index
+    @posts = Post.all
+    json_response(@posts)
+  end
+
   # POST /posts
   def create
     @post = Post.create!(params.permit(:title, :submited_by, :content))
-    render json: @post, status: :created
+    json_response(@post, :created)
+  end
+
+  def json_response(object, status = :ok)
+    render json: object, status: status
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,14 +4,43 @@ RSpec.describe 'Blog Posts API.', type: :request do
   # Test suite for POST /posts
   describe 'The POST /posts endpoint.' do
     let(:post_attributes) { { title: 'This is TDD',
-                              created_by: 'jpsoares106@gmail.com',
+                              submited_by: 'jpsoares106@gmail.com',
                               content: '# Lorem Ipsum' } }
 
     context 'When /posts is called with blog post attributes' do
       before { post '/posts', params: post_attributes }
 
-      it 'should create a blog post' do
+      it 'should create a blog post with the given title' do
         expect(json['title']).to eq(post_attributes[:title])
+      end
+
+      it 'should create a blog post with the given created_by' do
+        expect(json['submited_by']).to eq(post_attributes[:submited_by])
+      end
+
+      it 'should create a blog post with the given content' do
+        expect(json['content']).to eq(post_attributes[:content])
+      end
+    end
+
+    context 'When GET /posts is called after submiting a post' do
+      before { post '/posts', params: post_attributes
+               get '/posts'}
+
+      it 'should retrieve the submited post' do
+        expect(json).not_to be_empty
+      end
+
+      it 'should retrieve the submited post with title' do
+        expect(json.first['title']).to eq(post_attributes[:title])
+      end
+
+      it 'should retrieve the submited post with submited_by attribute' do
+        expect(json.first['submited_by']).to eq(post_attributes[:submited_by])
+      end
+
+      it 'should retrieve the submited post with content' do
+        expect(json.first['content']).to eq(post_attributes[:content])
       end
     end
   end
